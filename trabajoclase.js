ae1e0a0bcb8009811cfeb967df2904a4c32b524c
tefiavalos <emlavalos92@gmail.com>
2019-10-21 19:10:27 -0300

    trabajo de la clase js

---
+++ trabajoclase.js
@@ -0,0 +1,115 @@
+//ejercicio27
+
+let numero1
+numero1 = 3550
+
+let numero2
+
+numero2 = 4880
+
+let resultado = numero1 + numero2
+
+console.log (resultado)
+
+resultado = numero1 - numero2
+
+console.log (resultado)
+
+resultado = numero1 * numero2
+
+console.log (resultado)
+
+resultado = numero1 / numero2
+
+console.log (resultado)
+
+resultado = numero1 % numero2
+
+console.log (resultado)
+
+//ejercicio28
+
+let numero = 9
+
+let resultado2 = numero * 1
+
+console.log (resultado2)
+
+resultado2 = numero * 2
+
+console.log (resultado2)
+
+resultado2 = numero * 3
+
+console.log (resultado2)
+
+resultado2 = numero * 4
+
+console.log (resultado2)
+
+resultado2 = numero * 5
+
+console.log (resultado2)
+
+resultado2 = numero * 6
+
+console.log (resultado2)
+
+resultado2 = numero * 7
+
+console.log (resultado2)
+
+resultado2 = numero * 8
+
+console.log (resultado2)
+
+resultado2 = numero * 9
+
+console.log (resultado2)
+
+resultado2 = numero * 10
+
+console.log (resultado2)
+
+//ejercicio29
+
+let longitudLado = 10
+
+let perimetro = 10 * 4
+
+console.log (perimetro)
+
+//ejercicio30
+
+let longitud = 5
+
+let areaCuadrado = 5 ** 2
+
+console.log (areaCuadrado)
+
+//ejercicio31
+
+let ladoUno = 10
+
+let ladoDos = 20
+
+let ladoTres = 5
+
+let perimetroTriangulo = ladoUno + ladoDos + ladoTres
+
+console.log (perimetroTriangulo)
+
+//ejercicio32
+
+let altura = 10
+
+let base = 4
+
+let resultadoPerimetro = altura + base
+
+let resultadoArea = altura * base
+
+console.log (resultadoPerimetro)
+
+console.log (resultadoArea)
+
